e1cd8b92c89a189109a4bf906fd24bf78e374493
rangpurblog <rangpurblog@gmail.com>
2025-12-29 13:26:29 +0600

    clone voice list show done

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, UploadFile, Form
+from fastapi.staticfiles import StaticFiles
 from app.clone_voice import clone_voice
 from app.voice_service import generate_voice
 from app.clone_voice import delete_voice
-
+from app.utils import get_user_voices
 
 app = FastAPI()
+# app.mount("/voices", StaticFiles(directory="voices"), name="voices")
 
 @app.post("/clone-voice")
 async def clone(
@@ -13,6 +15,16 @@ async def clone(
     voice_name: str = Form(...)
 ):
     return clone_voice(audio, user_id, voice_name)
+
+@app.get("/voices/{user_id}")
+def list_user_voices(user_id: str):
+    voices = get_user_voices(user_id)
+
+    return {
+        "user_id": user_id,
+        "voices": voices
+    }
+
 
 @app.post("/delete-voice")
 async def delete_voice_api(
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,8 @@
 from pydub import AudioSegment
 import os
+import json
+
+BASE_DIR = "voices"
 
 def wav_to_mp3(wav_path: str) -> str:
     if not wav_path.endswith(".wav"):
@@ -11,3 +14,35 @@ def wav_to_mp3(wav_path: str) -> str:
     audio.export(mp3_path, format="mp3")
 
     return mp3_path
+
+
+def get_user_voices(user_id: str):
+    user_dir = os.path.join(BASE_DIR, user_id)
+
+    if not os.path.exists(user_dir):
+        return []
+
+    voices = []
+
+    for voice_id in os.listdir(user_dir):
+        voice_dir = os.path.join(user_dir, voice_id)
+        meta_path = os.path.join(voice_dir, "meta.json")
+        ref_path = os.path.join(voice_dir, "ref.wav")
+
+        if not os.path.exists(meta_path):
+            continue
+
+        with open(meta_path, "r") as f:
+            meta = json.load(f)
+
+        voices.append({
+            "voice_id": voice_id,
+            "display_name": meta.get(
+                "voice_name",
+                voice_id.replace("_", " ").title()
+            ),
+            "public": meta.get("public", False),
+            "audio_url": f"/voices/{user_id}/{voice_id}/ref.wav"
+        })
+
+    return voices
